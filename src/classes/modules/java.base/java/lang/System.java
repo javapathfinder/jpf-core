4b2f06bb3d95e17d89d6e41a53520684c6accb7a
Gayan Weerakutti <gayan@linuxdeveloper.space>
2018-03-17 19:15:55 +0530

    Refactor references to sun.misc.JavaLangAccess
    This fixes:
    
    [javac] src/classes/modules/java.base/java/lang/System.java:26: error: cannot find symbol
    [javac] import sun.misc.JavaLangAccess;
    [javac]                ^
    [javac]   symbol:   class JavaLangAccess
    [javac]   location: package sun.misc
    
    and subsequent errors:
    
    [javac] src/classes/modules/java.base/java/lang/System.java:68: error: cannot find symbol
    [javac]   static JavaLangAccess createJavaLangAccess () {
    [javac]          ^
    [javac]   symbol:   class JavaLangAccess
    [javac]   location: class System
    
    [javac] src/classes/modules/java.base/java/lang/System.java:69: error: cannot find symbol
    [javac]     return new JavaLangAccess(){
    [javac]                ^
    [javac]   symbol:   class JavaLangAccess
    [javac]   location: class System
    
    Fixes: #33

--- src/classes/modules/java.base/java/lang/System.java
+++ src/classes/modules/java.base/java/lang/System.java
@@ -23,7 +23,7 @@ import java.nio.channels.Channel;
 import java.util.Map;
 import java.util.Properties;
 
-import sun.misc.JavaLangAccess;
+import jdk.internal.misc.JavaLangAccess;
 import sun.misc.SharedSecrets;
 import sun.nio.ch.Interruptible;
 import sun.reflect.ConstantPool;
