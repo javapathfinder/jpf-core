5cfe5f6b3052fc76ef16c15ad11a177c479c1920
Cyrille Artho <artho@kth.se>
2018-11-23 13:50:45 +0100

    Fixed behavior of division by 0 on doubles (should be Infinity), added tests.

---
+++ src/tests/gov/nasa/jpf/test/java/lang/DoubleTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * The Java Pathfinder core (jpf-core) platform is licensed under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package gov.nasa.jpf.test.java.lang;
+
+import gov.nasa.jpf.util.test.TestJPF;
+
+import org.junit.Test;
+
+/**
+ * double related tests
+ */
+public class DoubleTest extends TestJPF {
+
+  @Test
+  public void testDivision() {
+    if (verifyNoPropertyViolation()) {
+      double i = 0;
+      double j = 10 / i;
+      assert(Double.valueOf(j).isInfinite());
+    }
+  }
+}
