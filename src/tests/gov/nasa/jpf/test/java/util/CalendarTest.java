137d7d0b8c872953a2e5469fe8c598e4dbf48802
fresheed <ok.john.rus@gmail.com>
2019-03-27 15:19:36 +0300

    added comment and main() method which can be called by RunJPF utility

--- src/tests/gov/nasa/jpf/test/java/util/CalendarTest.java
+++ src/tests/gov/nasa/jpf/test/java/util/CalendarTest.java
@@ -3,8 +3,14 @@ package gov.nasa.jpf.test.java.util;
 import gov.nasa.jpf.util.test.TestJPF;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 import java.util.Calendar;
 
+// To reproduce bad behavior, run these tests on JPF VM: first should pass, second should fail
+// On HotSpot both tests pass
 public class CalendarTest extends TestJPF {
 
     @Test
@@ -21,6 +27,19 @@ public class CalendarTest extends TestJPF {
         cal.set(Calendar.HOUR_OF_DAY, 23);
         Assert.assertEquals(2014, cal.get(Calendar.YEAR));
         Assert.assertEquals(23, cal.get(Calendar.HOUR_OF_DAY));
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(CalendarTest.class);
+        System.out.println("Tests run: " + result.getRunCount());
+        if (result.wasSuccessful()){
+            System.out.println("Success");
+        } else {
+            for (Failure failure: result.getFailures()){
+                System.out.println("Error: "+ failure.getMessage() + "\n  "
+                        +failure.getDescription() + "\n  " + failure.getException());
+            }
+        }
     }
 
 }
