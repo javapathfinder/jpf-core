d9f6accb119960c1fecb15fb33f6c2008e4bb559
Manish <manish3499@gmail.com>
2019-06-06 22:20:30 +0530

    Increase the range of m in VefifyChoiceTest.java

--- src/examples/VerifyChoiceTest.java
+++ src/examples/VerifyChoiceTest.java
@@ -3,12 +3,12 @@ import gov.nasa.jpf.vm.Verify;
 public class VerifyChoiceTest {
 	
 	static void main(String[] args) {
-		int m = Verify.getInt(1, 2);
+		int m = Verify.getInt(1, 11);
 		System.out.println("m = " + m);
 		int n = Verify.getInt(2, 3);
 		System.out.println("n = " + n);
 		System.out.println("m * n =" + m * n);
-		assert (n < 5); // fails (so we get a trace)
+		//assert ( (m * n) < 5); // fails (so we get a trace)
    }
 	
 	
