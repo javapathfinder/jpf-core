6cf657b158f6db0ea7e1927ae91e33c33d9035ac
yuvaraj-anbarasan <yuvaraj.klei@gmail.com>
2020-08-14 13:34:15 +0530

    Add test case to check if two long arrays are equal

--- src/tests/gov/nasa/jpf/test/java/util/ArraysTest.java
+++ src/tests/gov/nasa/jpf/test/java/util/ArraysTest.java
@@ -1,10 +1,22 @@
 package gov.nasa.jpf.test.java.util;
 
+import java.util.Arrays;
+
+import org.junit.Test;
+
 import gov.nasa.jpf.util.test.TestJPF;
 
 /**
  * Test for host JVM Array class operations.
  */
 public class ArraysTest extends TestJPF {
-    
+    @Test
+    public void testLongArrayEquals() {
+        if (verifyNoPropertyViolation()) {
+            long[] arr1 = {1, 2};
+            long[] arr2 = {1, 2};
+        
+            assertTrue(Arrays.equals(arr1, arr2));
+        }
+    }
 }
