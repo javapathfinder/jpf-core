1e1d85ce1fcdbf7d889f75a2ab095113b7e58fb9
yuvaraj-anbarasan <yuvaraj.klei@gmail.com>
2020-08-14 13:32:10 +0530

    Add test for Arrays operations

---
+++ src/tests/gov/nasa/jpf/test/java/util/ArraysTest.java
@@ -0,0 +1,10 @@
+package gov.nasa.jpf.test.java.util;
+
+import gov.nasa.jpf.util.test.TestJPF;
+
+/**
+ * Test for host JVM Array class operations.
+ */
+public class ArraysTest extends TestJPF {
+    
+}
