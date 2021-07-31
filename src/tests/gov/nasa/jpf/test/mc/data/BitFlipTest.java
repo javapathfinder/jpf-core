a3fa033ccedb9063cc1af02bcdf059c99f7f743f
Luke Yi <lukeyi@pku.edu.cn>
2021-07-31 20:15:42 +0800

    resolve issue #9: add 5 more complex tests for bit flips

--- src/tests/gov/nasa/jpf/test/mc/data/BitFlipTest.java
+++ src/tests/gov/nasa/jpf/test/mc/data/BitFlipTest.java
@@ -263,6 +263,179 @@ public class BitFlipTest extends TestJPF {
   }
 
   @Test
+  public void testCommandLineOptionSuppressesAnnotation() {
+
+    if (!isJPFRun()){
+      Verify.resetCounter(0);
+    }
+
+    if (verifyNoPropertyViolation("+listener=gov.nasa.jpf.listener.BitFlipListener",
+                "+bitflip.localvars=foobar",
+                "+bitflip.foobar.method=gov.nasa.jpf.test.mc.data.BitFlipTest.testCommandLineOptionSuppressesAnnotation()",
+                "+bitflip.foobar.name=d")){
+      System.out.println("command line option suppresses annotation test");
+      @BitFlip(2) int d = 0;
+      // though the annotation wants to flip 2 bits of d, the command line argument specifies to only flip 1 bit (by default)
+      System.out.print("d = ");
+      System.out.println(d);
+      checkBitFlip(d);
+    } else {
+      assert Verify.getCounter(0) == -1;
+    }
+  }
+
+  @Test
+  public void testCombineCommandLineOptionAndAnnotation() {
+
+    if (!isJPFRun()){
+      for (int i = 0; i < 32 * 32; ++i) {
+        Verify.setBitInBitSet(0, i, false);
+      }
+    }
+
+    if (verifyNoPropertyViolation("+listener=gov.nasa.jpf.listener.BitFlipListener",
+                "+bitflip.localvars=foobar",
+                "+bitflip.foobar.method=gov.nasa.jpf.test.mc.data.BitFlipTest.testCombineCommandLineOptionAndAnnotation()",
+                "+bitflip.foobar.name=d")){
+      System.out.println("combine command line option and annotation test");
+      int d = 0;
+      System.out.print("d = ");
+      System.out.println(d);
+      annotatedStaticField = 0;
+      System.out.print("annotatedStaticField = ");
+      System.out.println(annotatedStaticField);
+      // check that 32 * 32 cases are all covered exactly once
+      int bitNumber1 = 0, bitNumber2 = 0;
+      for (int i = 0; i < 32; ++i) {
+        if (d == (1 << i)) {
+          bitNumber1 = i;
+        }
+        if (annotatedStaticField == (1 << i)) {
+          bitNumber2 = i;
+        }
+      }
+      assert Verify.getBitInBitSet(0, 32 * bitNumber1 + bitNumber2) == false;
+      Verify.setBitInBitSet(0, 32 * bitNumber1 + bitNumber2, true);
+    } else {
+      for (int i = 0; i < 32 * 32; ++i) {
+        assert Verify.getBitInBitSet(0, i) == true;
+      }
+    }
+  }
+
+  @Test
+  public void testTwoCommandLineOptions() {
+
+    if (!isJPFRun()){
+      for (int i = 0; i < 32 * 32; ++i) {
+        Verify.setBitInBitSet(0, i, false);
+      }
+    }
+
+    if (verifyNoPropertyViolation("+listener=gov.nasa.jpf.listener.BitFlipListener",
+                "+bitflip.localvars=foo,bar",
+                "+bitflip.foo.method=gov.nasa.jpf.test.mc.data.BitFlipTest.testTwoCommandLineOptions()",
+                "+bitflip.foo.name=d1",
+                "+bitflip.bar.method=gov.nasa.jpf.test.mc.data.BitFlipTest.testTwoCommandLineOptions()",
+                "+bitflip.bar.name=d2")){
+      System.out.println("two command line options test");
+      int d1 = 0;
+      System.out.print("d1 = ");
+      System.out.println(d1);
+      int d2 = 0;
+      System.out.print("d2 = ");
+      System.out.println(d2);
+      // check that 32 * 32 cases are all covered exactly once
+      int bitNumber1 = 0, bitNumber2 = 0;
+      for (int i = 0; i < 32; ++i) {
+        if (d1 == (1 << i)) {
+          bitNumber1 = i;
+        }
+        if (d2 == (1 << i)) {
+          bitNumber2 = i;
+        }
+      }
+      assert Verify.getBitInBitSet(0, 32 * bitNumber1 + bitNumber2) == false;
+      Verify.setBitInBitSet(0, 32 * bitNumber1 + bitNumber2, true);
+    } else {
+      for (int i = 0; i < 32 * 32; ++i) {
+        assert Verify.getBitInBitSet(0, i) == true;
+      }
+    }
+  }
+
+  @Test
+  public void testTwoAnnotations() {
+
+    if (!isJPFRun()){
+      for (int i = 0; i < 32 * 32; ++i) {
+        Verify.setBitInBitSet(0, i, false);
+      }
+    }
+
+    if (verifyNoPropertyViolation("+listener=gov.nasa.jpf.listener.BitFlipListener")){
+      System.out.println("two annotations test");
+      annotatedStaticField = 0;
+      System.out.print("annotatedStaticField = ");
+      System.out.println(annotatedStaticField);
+      annotatedInstanceField = 0;
+      System.out.print("annotatedInstanceField = ");
+      System.out.println(annotatedInstanceField);
+      // check that 32 * 32 cases are all covered exactly once
+      int bitNumber1 = 0, bitNumber2 = 0;
+      for (int i = 0; i < 32; ++i) {
+        if (annotatedStaticField == (1 << i)) {
+          bitNumber1 = i;
+        }
+        if (annotatedInstanceField == (1 << i)) {
+          bitNumber2 = i;
+        }
+      }
+      assert Verify.getBitInBitSet(0, 32 * bitNumber1 + bitNumber2) == false;
+      Verify.setBitInBitSet(0, 32 * bitNumber1 + bitNumber2, true);
+    } else {
+      for (int i = 0; i < 32 * 32; ++i)
+        assert Verify.getBitInBitSet(0, i) == true;
+    }
+  }
+
+  @Test
+  public void testFlipTwoBits() {
+
+    if (!isJPFRun()){
+      for (int i = 0; i < (32 * 31) / 2; ++i) {
+        Verify.setBitInBitSet(0, i, false);
+      }
+    }
+
+    if (verifyNoPropertyViolation("+listener=gov.nasa.jpf.listener.BitFlipListener")){
+      @BitFlip(2) int d;
+      d = 0;
+      System.out.print("d = ");
+      System.out.println(d);
+      // check that C(32, 2) cases are all covered exactly once
+      int bitNumber1 = -1, bitNumber2 = -1, tmp = d;
+      for (int i = 0; i < 32; ++i) {
+        if ((d >> i & 1) == 1) {
+          if (bitNumber1 == -1) {
+            bitNumber1 = i;
+          } else {
+            bitNumber2 = i;
+          }
+          tmp ^= (1 << i);
+        }
+      }
+      assert bitNumber1 != -1 && bitNumber2 != -1 && tmp == 0;
+      int case_number = bitNumber2 * (bitNumber2 - 1) / 2 + bitNumber1;
+      assert Verify.getBitInBitSet(0, case_number) == false;
+      Verify.setBitInBitSet(0, case_number, true);
+    } else {
+      for (int i = 0; i < (32 * 31) / 2; ++i)
+        assert Verify.getBitInBitSet(0, i) == true;
+    }
+  }
+
+  @Test
   public void testBitFlipAPI() {
 
     if (!isJPFRun()){
