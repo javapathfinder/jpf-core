328e598996cb05028465d0620a2a05c581022f2f
Gaurang Kudale <64984006+gaurangkudale@users.noreply.github.com>
2021-11-17 21:29:41 +0530

    Fixed some syntax Errors

--- src/classes/java/lang/ThreadLocal.java
+++ src/classes/java/lang/ThreadLocal.java
@@ -105,15 +105,13 @@ public class ThreadLocal<T> {
     class threadSafeFormatter{
       public static ThreadLocal<SimpleDateFormat> df = ThreadLocal.withInitial(() 
           -> new SimpleDateFormat("yyyy-MM-dd"));
-  }
-  
   public class App  {
       public static void main(String[] args) throws Exception {
           threadSafeFormatter tf = new threadSafeFormatter();
           Thread t1 = new Thread();
           t1.start();
       }
-  
+    }
       public static String birthDate(int userId){
           Date birthdDate = new Date(userId);
           final SimpleDateFormat df = threadSafeFormatter.df.get();
