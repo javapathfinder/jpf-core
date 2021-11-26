913902fcfe83d7e2e37f3ec397b3a0b4b189da30
Gaurang Kudale <64984006+gaurangkudale@users.noreply.github.com>
2021-11-26 22:29:00 +0530

    code changes

--- src/tests/StreamTest/ExampleTest.java
+++ src/tests/StreamTest/ExampleTest.java
@@ -2,7 +2,9 @@ import org.junit.Test;
 
 public class ExampleTest {
     @Test
+    public static void Testing() {
     Example ex = new Example();
     int x = ex.doSomthing();
     assertEquals(x, 0);
+    }
 }
