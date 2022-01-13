c58bc523724140e05c5fca010261478ae3987de4
Gaurang Kudale <64984006+gaurangkudale@users.noreply.github.com>
2022-01-13 19:05:05 +0530

    added import statments

--- src/tests/java8/StreamTest.java
+++ src/tests/java8/StreamTest.java
@@ -4,6 +4,8 @@ import gov.nasa.jpf.util.test.TestJPF;
 import java.util.function.Supplier;
 import org.junit.Test;
 import java.util.List
+import java.util.stream;
+import java.util.map;
 
 public class StreamTest {
     public static List<String> allToUpperCase(List<String> words) {
