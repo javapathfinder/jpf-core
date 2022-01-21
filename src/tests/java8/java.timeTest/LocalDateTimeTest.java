b93fae79426a5be1abb2cfcce80ad21a76467cc3
Gaurang Kudale <64984006+gaurangkudale@users.noreply.github.com>
2022-01-21 19:13:55 +0530

    added method

--- src/tests/java8/java.timeTest/LocalDateTimeTest.java
+++ src/tests/java8/java.timeTest/LocalDateTimeTest.java
@@ -32,5 +32,8 @@ public class LocalDateTimeTest {
         assertThat(eastCoastTime.get(ChronoField.CLOCK_HOUR_OF_DAY) - centralTime.get(ChronoField.CLOCK_HOUR_OF_DAY),is(1));
 
     }
+    private Object is(String string) {
+        return null;
+    }
 
 }
