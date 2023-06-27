46b5f283038e640d94d49b7ab805f4e74f8e35ef
Daohan Qu <quadhier@163.com>
2023-06-27 20:02:01 +0800

    Implement StackWalker API for JPF on Java 11 (#360)
    * Add StackFrameInfo for StackWalker to work
    
    * Implement native methods to support StackWalker
    
    * Add unit tests
    
    * Add some clarifications
    
    * Remove unnecessary line breaks in for-statements
    
    * Add implementation of toStackTraceElement() and a unit test for it
    
    * Add code to set 'methodType' field and a unit test for that
    
    * Fix a typo
    
    * Change an argument of a unit test
    
    * Fix a failing test

--- src/tests/gov/nasa/jpf/test/java/lang/StackWalkerTest.java
+++ src/tests/gov/nasa/jpf/test/java/lang/StackWalkerTest.java
@@ -21,6 +21,9 @@ import gov.nasa.jpf.util.test.TestJPF;
 
 import org.junit.Test;
 
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,15 +47,12 @@ public class StackWalkerTest extends TestJPF {
               .collect(Collectors.toList()));
 
       Class<?> callerCls = callerClasses.get(0); // foo()
-      System.out.println("-- declaring class of the top frame of the stack = " + callerCls);
       assertTrue(callerCls.getName().equals("gov.nasa.jpf.test.java.lang.StackWalkerTest$MyClass"));
 
       callerCls = callerClasses.get(1); // bar()
-      System.out.println("-- StackFrame[1].getDeclaringClass = " + callerCls);
       assertTrue(callerCls.getName().equals("gov.nasa.jpf.test.java.lang.StackWalkerTest$MyClass"));
 
       callerCls = callerClasses.get(2); // callIt()
-      System.out.println("-- StackFrame[2].getDeclaringClass = " + callerCls);
       assertTrue(callerCls.getName().equals("gov.nasa.jpf.test.java.lang.StackWalkerTest"));
     }
   }
@@ -66,6 +66,107 @@ public class StackWalkerTest extends TestJPF {
   public void testCallerClass() {
     if (verifyNoPropertyViolation()){
       callIt();
+    }
+  }
+
+  void reflectionCallee() {
+    // Case 1, don't show reflect frames
+    // By default, reflect frames are filtered out
+    final StackWalker walker1 = StackWalker.getInstance();
+    List<String> methodNames1 = walker1.walk(s -> s
+        .map(StackWalker.StackFrame::getMethodName)
+        .collect(Collectors.toList()));
+    assertTrue(methodNames1.get(0).equals("reflectionCallee"));
+    assertTrue(methodNames1.get(1).equals("testShowReflectFrame"));
+
+    // Case 2, show reflect frames
+    final StackWalker walker2 = StackWalker.getInstance(StackWalker.Option.SHOW_REFLECT_FRAMES);
+    List<String> methodNames2 = walker2.walk(s -> s
+        .map(StackWalker.StackFrame::getMethodName)
+        .collect(Collectors.toList()));
+    // Since implementation of reflect invocation is JVM's internal detail and
+    // could be different across different JVMs. We need to loose the constraints.
+    // But there should be at least two `Method::invoke`s on stack (One for
+    // reflectionCallee() reflection call and another for unit test reflection call).
+    System.out.println(methodNames2);
+    assertTrue(Collections.frequency(methodNames2, "invoke") >= 2);
+  }
+
+  @Test
+  public void testShowReflectFrame() throws Exception {
+    if (verifyNoPropertyViolation()){
+      Method reflectCallee = StackWalkerTest.class.getDeclaredMethod("reflectionCallee");
+      reflectCallee.invoke(new StackWalkerTest());
+    }
+  }
+
+  @Test
+  public void testStackWalkerInCaseOfChoiceGenerator() {
+    if (verifyNoPropertyViolation()){
+      Thread t1 = new Thread() {
+        @Override
+        public void run() {
+          callIt();
+        }
+      };
+      Thread t2 = new Thread() {
+        @Override
+        public void run() {
+          callIt();
+        }
+      };
+      t1.start();
+      t2.start();
+    }
+  }
+
+  void deepCallRecur(int n, int depth) {
+    if (n == 1) {
+      final StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+      List<String> methods = walker.walk(s -> s
+          .map(StackWalker.StackFrame::getMethodName)
+          .filter(m -> m.equals("deepCallRecur"))
+          .collect(Collectors.toList()));
+      assertEquals(methods.size(), depth);
+    } else {
+      deepCallRecur(n - 1, depth);
+    }
+  }
+
+  void deepCall(int depth) {
+    deepCallRecur(depth, depth);
+  }
+
+  @Test
+  public void testDeepStack() {
+    if (verifyNoPropertyViolation()) {
+      deepCall(5);
+    }
+  }
+
+  @Test
+  public void testToStackTraceElement() {
+    if (verifyNoPropertyViolation()) {
+      StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+      String thisMethodName = walker.walk(s -> s
+          .map(StackWalker.StackFrame::toStackTraceElement)
+          .map(StackTraceElement::getMethodName)
+          .collect(Collectors.toList()))
+          .get(0);
+      assertTrue(thisMethodName.equals("testToStackTraceElement"));
+    }
+  }
+
+  @Test
+  public void testGetMethodType() {
+    if (verifyNoPropertyViolation()) {
+      StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+      MethodType thisMethodType = walker.walk(s -> s
+          .limit(1)
+          .map(StackWalker.StackFrame::getMethodType)
+          .collect(Collectors.toList()))
+          .get(0);
+      assertTrue(thisMethodType.equals(MethodType.fromMethodDescriptorString("()V", null)));
     }
   }
 }
