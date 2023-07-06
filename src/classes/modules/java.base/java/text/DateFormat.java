f376135d6db1d9f4c32ee9bc7a12dac4f98edf40
varad64 <varad23711@users.noreply.github.com>
2023-07-07 01:19:25 +0530

    Addresses requested changes in #363 to fix (Simple)DateFormatTest as part of #274

--- src/classes/modules/java.base/java/text/DateFormat.java
+++ src/classes/modules/java.base/java/text/DateFormat.java
@@ -1,3 +1,41 @@
+/*
+ * Copyright (c) 1996, 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * (C) Copyright Taligent, Inc. 1996 - All Rights Reserved
+ * (C) Copyright IBM Corp. 1996 - All Rights Reserved
+ *
+ *   The original version of this source code and documentation is copyrighted
+ * and owned by Taligent, Inc., a wholly-owned subsidiary of IBM. These
+ * materials are provided under terms of a License Agreement between Taligent
+ * and Sun. This technology is protected by multiple US and International
+ * patents. This notice and attribution to Taligent may not be removed.
+ *   Taligent is a registered trademark of Taligent, Inc.
+ *
+ */
+
 package java.text;
 
 import java.util.Calendar;
@@ -7,77 +45,71 @@ import java.util.TimeZone;
 
 public abstract class DateFormat extends Format {
 
-    static int nInstances;
-    private int id = nInstances++;
-    public static final int FULL = 0;
-    public static final int LONG = 1;
-    public static final int MEDIUM = 2;
-    public static final int SHORT = 3;
-    public static final int DEFAULT = SHORT;
-    protected Calendar calendar;
+  public static final int FULL = 0;
+  public static final int LONG = 1;
+  public static final int MEDIUM = 2;
+  public static final int SHORT = 3;
+  public static final int DEFAULT = SHORT;
+  protected Calendar calendar;
 
-    public DateFormat() {}
+  public DateFormat() {}
 
-    public static final DateFormat getDateTimeInstance(int dateStyle, int timeStyle, Locale locale) {
-        DateFormat dateFormat = getDT(dateStyle, timeStyle, 3, locale);
-        return dateFormat;
+  public static final DateFormat getDateTimeInstance(int dateStyle, int timeStyle, Locale locale) {
+    DateFormat dateFormat = getDT(dateStyle, timeStyle, 3, locale);
+    return dateFormat;
+  }
+
+  private static DateFormat getDT(int dateStyle, int timeStyle, int style, Locale locale) {
+    return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  }
+
+  public DateFormat getDateTimeInstance(int dateStyle, int timeStyle) {
+    return getDateTimeInstance(dateStyle, timeStyle, Locale.getDefault(Locale.Category.FORMAT));
+  }
+
+  public void setLenient(boolean lenient) {
+    if(calendar == null) {
+      calendar = Calendar.getInstance(TimeZone.getDefault(), Locale.getDefault());
     }
+    calendar.setLenient(lenient);
+  }
 
-    private static DateFormat getDT(int dateStyle, int timeStyle, int flags, Locale locale) {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  public void setTimeZone(TimeZone zone) {
+    if(calendar == null) {
+      calendar = Calendar.getInstance(TimeZone.getDefault(), Locale.getDefault());
     }
+    calendar.setTimeZone(zone);
+  }
 
-    public DateFormat getDateTimeInstance(int dateStyle, int timeStyle) {
-        return getDateTimeInstance(dateStyle, timeStyle, Locale.getDefault(Locale.Category.FORMAT));
-    }
+  public TimeZone getTimeZone() {
+    return calendar.getTimeZone();
+  }
 
-    public void setLenient(boolean lenient)
-    {
-        if(calendar == null) {
-            calendar = Calendar.getInstance(TimeZone.getDefault(), Locale.getDefault());
-        }
-        calendar.setLenient(lenient);
-    }
+  public static Locale[] getAvailableLocales() {
+    return Locale.getAvailableLocales();
+  }
 
-    public void setTimeZone(TimeZone zone)
-    {
-        if(calendar == null) {
-            calendar = Calendar.getInstance(TimeZone.getDefault(), Locale.getDefault());
-        }
-        calendar.setTimeZone(zone);
-    }
+  public final String format(Date date) {
+    return format(date, new StringBuffer(), DontCareFieldPosition.INSTANCE).toString();
+  }
 
-    public TimeZone getTimeZone() {return calendar.getTimeZone();}
+  public Date parse(String source) throws ParseException {
+    ParsePosition pos = new ParsePosition(0);
+    Date result = parse(source, pos);
+    if (pos.index == 0)
+      throw new ParseException("Unable to parse date: \"" + source + "\"" , pos.errorIndex);
+    return result;
+  }
 
-    public static Locale[] getAvailableLocales() {
-        return Locale.getAvailableLocales();
-    }
+  public abstract Date parse(String source, ParsePosition pos);
 
-    public final String format(Date date)
-    {
-        return format(date, new StringBuffer(),
-                DontCareFieldPosition.INSTANCE).toString();
-    }
+  @Override
+  public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+    return null;
+  }
 
-    public Date parse(String source) throws ParseException
-    {
-        ParsePosition pos = new ParsePosition(0);
-        Date result = parse(source, pos);
-        if (pos.index == 0)
-            throw new ParseException("Unparseable date: \"" + source + "\"" ,
-                    pos.errorIndex);
-        return result;
-    }
-
-    public abstract Date parse(String source, ParsePosition pos);
-
-    @Override
-    public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-        return null;
-    }
-
-    @Override
-    public Object parseObject(String source, ParsePosition pos) {
-        return null;
-    }
+  @Override
+  public Object parseObject(String source, ParsePosition pos) {
+    return null;
+  }
 }
--- src/classes/modules/java.base/java/util/ServiceLoader.java
+++ src/classes/modules/java.base/java/util/ServiceLoader.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * The Java Pathfinder core (jpf-core) platform is licensed under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package java.util;
 
 import java.io.BufferedReader;
@@ -8,16 +26,39 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.function.Supplier;
 
+/**
+ * MJI model class for java.util.ServiceLoader
+ *
+ * Model class whose primary objective is to eliminate
+ * the ServiceLoader class' dependencies on any internal
+ * classes belonging to packages like <i>jdk.internal</i> or <i>sun</i>
+ * @param <S>
+ */
 public class ServiceLoader<S> implements Iterable<S> {
+    //service configuration file to configure service providers
     private static final String prefix = "META-INF/services/";
     private final Class<S> service;
-    private final ClassLoader classLoader;
+    private ClassLoader classLoader;
     private final Map<String, S> providers;
 
+    /**
+     * Represents a service provider located by {@code ServiceLoader}.
+     *
+     * @param  <S> The service type
+     * @since 9
+     * @spec JPMS
+     */
     public static interface Provider<S> extends Supplier<S> {
         Class<? extends S> type();
         @Override S get();
     }
+
+    /**
+     * Initializes a new instance of this class for locating service providers
+     * for the given service via the classloader
+     * @param service
+     * @param classLoader
+     */
 
     private ServiceLoader(Class service, ClassLoader classLoader) {
         this.service = service;
@@ -25,20 +66,71 @@ public class ServiceLoader<S> implements Iterable<S> {
         this.providers = new LinkedHashMap<>();
     }
 
+    /**
+     * Creates a new service loader for the given service type
+     *
+     * @param <S> the class of the service type
+     *
+     * @param service interface or abstract class representing the service
+     *
+     * @return A new service loader
+     */
     public static <S> ServiceLoader<S> load(Class<S> service) {
         return load(service, Thread.currentThread().getContextClassLoader());
     }
 
+    /**
+     *
+     * Creates a new service loader for the given service type. The
+     * service loader makes use of the given class loader as the starting
+     * point to locate service providers for the service.
+     *
+     * @param <S> the class of the service type
+     *
+     * @param service interface or abstract class representing the service
+     *
+     * @param loader the classloader to be used to load provider-configuration files
+     *               and provider classes
+     *
+     * @return A new service loader
+     */
     public static <S> ServiceLoader<S> load(Class<S> service, ClassLoader loader)
     {
         return new ServiceLoader<>(service, loader);
     }
 
+    /**
+     *
+     * Creates a new service loader for the given service type, using the
+     * {@linkplain ClassLoader#getPlatformClassLoader() platform class loader}
+     *
+     * <p> This convenience method is equivalent to: </p>
+     *
+     * <pre>{@code
+     *     ServiceLoader.load(service, ClassLoader.getPlatformClassLoader())
+     * }</pre>
+     *
+     *
+     * @param <S> the class of the service type
+     *
+     * @param service the interface or abstract class representing the service
+     *
+     * @return A new service loader
+     *
+     */
     public static <S> ServiceLoader<S> loadInstalled(Class<S> service) {
         ClassLoader cl = ClassLoader.getPlatformClassLoader();
         return new ServiceLoader<>(service, cl);
     }
 
+    /**
+     * Returns an iterator to lazily load and instantiate the
+     * available providers of this loader's service
+     *
+     * @return An iterator that lazily loads providers for this
+     * loader's service
+     *
+     */
     @Override
     public Iterator<S> iterator() {
         return new LazyIterator();
@@ -53,6 +145,13 @@ public class ServiceLoader<S> implements Iterable<S> {
         public LazyIterator() {
             try {
                 String configFile = prefix + service.getName();
+                // ensure classloader is correcltly initialized before use
+                if(classLoader == null) {
+                    classLoader = Thread.currentThread().getContextClassLoader();
+                    if(classLoader == null) {
+                        classLoader = ClassLoader.getSystemClassLoader();
+                    }
+                }
                 // get enum of URLs matching the config file name.
                 configFiles = classLoader.getResources(configFile);
                 providerNames = Collections.emptyIterator();
@@ -114,6 +213,7 @@ public class ServiceLoader<S> implements Iterable<S> {
             throw new UnsupportedOperationException();
         }
 
+        // Reads the providers from the provided config file
         private Iterator<String> getProviders(URL url) throws IOException {
             try(InputStream is = url.openStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"))) {
