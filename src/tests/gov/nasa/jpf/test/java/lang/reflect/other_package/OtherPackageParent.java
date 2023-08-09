030147b681f3f21e141fe76983e86aae7e991b14
Wen Zhang <zhangwen@cs.berkeley.edu>
2023-08-09 04:33:03 +0000

    More complete implementation of Field.isAccessible

---
+++ src/tests/gov/nasa/jpf/test/java/lang/reflect/other_package/OtherPackageParent.java
@@ -0,0 +1,9 @@
+package gov.nasa.jpf.test.java.lang.reflect.other_package;
+
+/**
+ * Used in gov.nasa.jpf.test.java.lang.reflect.FieldTest as a superclass.
+ */
+public class OtherPackageParent {
+    protected static int parentStaticProtectedField = 40;
+    protected int parentStaticField = 50;
+}
