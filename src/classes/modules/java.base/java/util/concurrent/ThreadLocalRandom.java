b6aa6181b7d08fb09812cbe2595a7c6797712098
quadhier <quadhier@163.com>
2023-08-11 20:32:02 +0800

    Add model class for ThreadLocalRandom

---
+++ src/classes/modules/java.base/java/util/concurrent/ThreadLocalRandom.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * The Java Pathfinder core (jpf-core) platform is licensed under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package java.util.concurrent;
+
+import java.util.Random;
+
+public class ThreadLocalRandom {
+
+  static Random rand = new Random();
+
+  static final ThreadLocalRandom instance = new ThreadLocalRandom();
+
+  public static final ThreadLocalRandom current() {
+    return instance;
+  }
+
+  static final int nextSecondarySeed() {
+    return rand.nextInt();
+  }
+}
