650975445b7e425b4e61cae0db7b5b99f6272e83
varad64 <varad23711@users.noreply.github.com>
2023-08-13 01:23:41 +0530

    Reindent using 2 spaces

--- src/tests/gov/nasa/jpf/test/java/util/StreamTest.java
+++ src/tests/gov/nasa/jpf/test/java/util/StreamTest.java
@@ -26,16 +26,16 @@ import org.junit.Test;
 import static java.util.Arrays.asList;
 
 public class StreamTest extends TestJPF {
-    public static List<String> allToUpperCase(List<String> words) {
-        return words.stream()
-                    .map(string -> string.toUpperCase())
-                    .collect(Collectors.toList());
-    }
+  public static List<String> allToUpperCase(List<String> words) {
+    return words.stream()
+        .map(string -> string.toUpperCase())
+        .collect(Collectors.toList());
+  }
 
-    @Test
-public void multipleWordsToUppercase() {
+  @Test
+  public void multipleWordsToUppercase() {
     List<String> input = Arrays.asList("a", "b", "hello");
     List<String> result = allToUpperCase(input);
     assertEquals(asList("A", "B", "HELLO"), result);
-    }
+  }
 }
