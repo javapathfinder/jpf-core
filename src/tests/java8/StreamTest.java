ebbc349ed9b9489de0305d4f2229c4b490f1a229
varad64 <varad23711@users.noreply.github.com>
2023-08-12 21:42:21 +0530

    Refactor StreamTest to add necessary imports

--- src/tests/java8/StreamTest.java
+++ src/tests/java8/StreamTest.java
@@ -2,11 +2,13 @@ package java8;
 
 import gov.nasa.jpf.util.test.TestJPF;
 
-import java.util.function.Supplier;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
+import static java.util.Arrays.asList;
 
-public class StreamTest {
+public class StreamTest extends TestJPF {
     public static List<String> allToUpperCase(List<String> words) {
         return words.stream()
                     .map(string -> string.toUpperCase())
