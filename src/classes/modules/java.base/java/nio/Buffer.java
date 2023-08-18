9efc54889fb7e62094922ce710514a31ae221e70
Varadraj Galgali <42909741+varad64@users.noreply.github.com>
2023-08-18 13:39:32 +0530

    Apply patch for #171, to add support for java.nio.Buffer (#409)
    * Apply patch for #171, to add support for java.nio.Buffer
    
    * Add support for CharBuffer, a unit test for ByteBuffer constructor and refactor the existing ByteBufferTest to BufferTest
    
    * Add CharBuffer to java/nio
    
    ---------
    
    Co-authored-by: varad64 <varad23711@users.noreply.github.com>

--- src/classes/modules/java.base/java/nio/Buffer.java
+++ src/classes/modules/java.base/java/nio/Buffer.java
@@ -6,76 +6,95 @@
  * The Java Pathfinder core (jpf-core) platform is licensed under the
  * Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
- *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package java.nio;
 
 public abstract class Buffer {
-	protected int position;
-	protected int capacity;
-	protected int limit;
 
-	public final int capacity() {
-		return capacity;
-	}
+  protected int position = 0;
+  protected int capacity;
+  protected int limit;
+  protected int mark = -1;
+  long address;
 
-	public final Buffer position(int newPosition) {
-		if ((newPosition<0)||(newPosition>limit)) {
-			throw new IllegalArgumentException("Illegal buffer position exception: "+newPosition);
-		}
-		this.position = newPosition;
-		return this;
-	}
 
-	public final int position() {
-		return position;
-	}
+  // Todo: There exists other missing methods in this class
+  // which may throw NoSuchMethodException errors to users
+  // For example checkBounds has yet to be implemented
+  Buffer(int mark, int pos, int lim, int cap) {
+    if (cap < 0)
+      throw new IllegalArgumentException("Negative capacity: " + cap);
+    this.capacity = cap;
+    limit(lim);
+    position(pos);
+    if (mark >= 0 && mark <= pos){
+      this.mark = mark;
+    }
+  }
 
-	public final int limit() {
-		return this.limit;
-	}
+  public final int capacity() {
+    return capacity;
+  }
 
-	public final Buffer limit(int newLimit) {
-		if ((newLimit<0)||(newLimit>capacity)) {
-			throw new IllegalArgumentException("Illegal buffer limit exception: "+newLimit);
-		}
-		this.limit = newLimit;
-		return this;
-	}
+  public Buffer position(int newPosition) {
+    if ((newPosition<0)||(newPosition>limit)) {
+      throw new IllegalArgumentException("Illegal buffer position exception: "+newPosition);
+    }
+    this.position = newPosition;
+    return this;
+  }
 
-	public final Buffer clear(){
-		position = 0;
-		limit = capacity;
-		return this;
-	}
+  public final int position() {
+    return position;
+  }
 
-	public final Buffer flip() {
-		limit = position;
-		position = 0;
-		return this;
-	}
+  public int limit() {
+    return this.limit;
+  }
 
-	public final Buffer rewind() {
-		position = 0;
-		return this;
-	}
+  public Buffer limit(int newLimit) {
+    if ((newLimit<0)||(newLimit>capacity)) {
+      throw new IllegalArgumentException("Illegal buffer limit exception: "+newLimit);
+    }
+    this.limit = newLimit;
+    return this;
+  }
 
-	public final int remaining() {
-		return limit-position;
-	}
+  public final Buffer clear(){
+    position = 0;
+    limit = capacity;
+    mark = -1;
+    return this;
+  }
 
-	public final boolean hasRemaining() {
-		return remaining()>0;
-	}
+  public Buffer flip() {
+    limit = position;
+    position = 0;
+    return this;
+  }
 
-	public abstract boolean hasArray();
+  public final Buffer rewind() {
+    position = 0;
+    return this;
+  }
 
-	public abstract Object array();
+  public final int remaining() {
+    return limit-position;
+  }
+
+  public final boolean hasRemaining() {
+    return remaining()>0;
+  }
+
+  public abstract boolean hasArray();
+
+  public abstract Object array();
 }
--- src/classes/modules/java.base/java/nio/ByteBuffer.java
+++ src/classes/modules/java.base/java/nio/ByteBuffer.java
@@ -6,283 +6,314 @@
  * The Java Pathfinder core (jpf-core) platform is licensed under the
  * Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
- *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package java.nio;
 
 public class ByteBuffer extends Buffer {
-	byte[] array;
+  byte[] array;
+  int offset;
 
-	public static ByteBuffer allocate(int i) {
-		if (i < 0) {
-			throw new IllegalArgumentException();
-		}
-		ByteBuffer newBuffer = new ByteBuffer(i);
-		return newBuffer;
-	}
+  public static ByteBuffer allocate(int i) {
+    if (i < 0) {
+      throw new IllegalArgumentException();
+    }
+    ByteBuffer newBuffer = new ByteBuffer(-1, 0, i, i, new byte[i], 0);
+    return newBuffer;
+  }
 
-	public static ByteBuffer allocateDirect(int capacity) {
-		return allocate(capacity);
-	}
+  public static ByteBuffer allocateDirect(int capacity) {
+    return allocate(capacity);
+  }
 
-	protected ByteBuffer(int i) {
-		capacity = i;
-		this.clear();
-		array = new byte[i];
-	}
+  ByteBuffer(int mark, int pos, int lim, int cap, byte[] hb, int offset) {
+    super(mark, pos, lim, cap);
+    this.array = hb;
+    this.offset = offset;
+  }
 
-	public ByteBuffer duplicate() {
-		ByteBuffer copy = new ByteBuffer(capacity);
-		copy.array = array;
-		return copy;
-	}
+  ByteBuffer(int mark, int pos, int lim, int cap) {
+    this(mark, pos, lim, cap, null, 0);
+  }
 
-	public ByteBuffer asReadOnlyBuffer() {
-		return duplicate();
-	}
+  public ByteBuffer position(int newPosition) {
+    super.position(newPosition);
+    return this;
+  }
 
-	public ByteBuffer slice() {
-		int remaining = limit - position;
-		ByteBuffer copy = new ByteBuffer(remaining);
-		copy.array = array;
-		return copy;
-	}
+  public ByteBuffer duplicate() {
+    ByteBuffer copy = new ByteBuffer(-1, 0, capacity, capacity, new byte[capacity], 0);
+    copy.array = array;
+    return copy;
+  }
 
-	public ByteBuffer put(byte b) {
-		if (position >= limit) {
-			throw new BufferOverflowException();
-		}
-		array[position] = b;
-		position++;
-		return this;
-	}
+  public ByteBuffer asReadOnlyBuffer() {
+    return duplicate();
+  }
 
-	public ByteBuffer put(int i, byte b) {
-		if ((i < 0) || (i >= limit)) {
-			throw new IndexOutOfBoundsException();
-		}
-		array[i] = b;
-		return this;
-	}
+  public ByteBuffer slice() {
+    int remaining = limit - position;
+    ByteBuffer copy = new ByteBuffer(-1, 0, remaining, remaining, new byte[remaining], 0);
+    copy.array = array;
+    return copy;
+  }
 
-	public ByteBuffer put(ByteBuffer src) {
-		if (src == this) {
-			throw new IllegalArgumentException();
-		}
+  public ByteBuffer put(byte b) {
+    if (position >= limit) {
+      throw new BufferOverflowException();
+    }
+    array[position] = b;
+    position++;
+    return this;
+  }
 
-		int srcRemaining = src.remaining();
-		if (srcRemaining > remaining()) {
-			throw new BufferOverflowException();
-		}
+  public ByteBuffer put(int i, byte b) {
+    if ((i < 0) || (i >= limit)) {
+      throw new IndexOutOfBoundsException();
+    }
+    array[i] = b;
+    return this;
+  }
 
-		System.arraycopy(src.array, src.position(), array, position, srcRemaining);
+  public ByteBuffer put(ByteBuffer src) {
+    if (src == this) {
+      throw new IllegalArgumentException();
+    }
 
-		src.position(src.position() + srcRemaining);
-		position(position + srcRemaining);
+    int srcRemaining = src.remaining();
+    if (srcRemaining > remaining()) {
+      throw new BufferOverflowException();
+    }
 
-		return this;
-	}
+    System.arraycopy(src.array, src.position(), array, position, srcRemaining);
 
-	public ByteBuffer put(byte[] bytes, int offset, int length) {
-		if ((offset | length | (offset + length) | (bytes.length - (offset + length))) < 0) {
-			throw new IndexOutOfBoundsException();
-		}
+    src.position(src.position() + srcRemaining);
+    position(position + srcRemaining);
 
-		if (length > remaining()) {
-			throw new BufferOverflowException();
-		}
+    return this;
+  }
 
-		System.arraycopy(bytes, offset, array, position, length);
-		position(position + length);
+  public ByteBuffer put(byte[] bytes, int offset, int length) {
+    if ((offset | length | (offset + length) | (bytes.length - (offset + length))) < 0) {
+      throw new IndexOutOfBoundsException();
+    }
 
-		return this;
-	}
+    if (length > remaining()) {
+      throw new BufferOverflowException();
+    }
 
-	public ByteBuffer put(byte[] bytes) {
-		return put(bytes, 0, bytes.length);
-	}
+    System.arraycopy(bytes, offset, array, position, length);
+    position(position + length);
 
-	public byte get() {
-		if (position >= limit) {
-			throw new BufferUnderflowException();
-		}
-		position++;
-		return array[position-1];
-	}
+    return this;
+  }
 
-	public byte get(int i) {
-		if ((i < 0) || (i >= limit)) {
-			throw new IndexOutOfBoundsException();
-		}
-		return array[i];
-	}
+  public ByteBuffer put(byte[] bytes) {
+    return put(bytes, 0, bytes.length);
+  }
 
-	public ByteBuffer get(byte[] bytes) {
-		return get(bytes, 0, bytes.length);
-	}
+  public byte get() {
+    if (position >= limit) {
+      throw new BufferUnderflowException();
+    }
+    position++;
+    return array[position-1];
+  }
 
-	public ByteBuffer get(byte[] bytes, int offset, int length) {
-		if ((offset | length | (offset + length) | (bytes.length - (offset + length))) < 0) {
-			throw new IndexOutOfBoundsException();
-		}
+  public byte get(int i) {
+    if ((i < 0) || (i >= limit)) {
+      throw new IndexOutOfBoundsException();
+    }
+    return array[i];
+  }
 
-		if (length > remaining()) {
-			throw new BufferUnderflowException();
-		}
+  public ByteBuffer get(byte[] bytes) {
+    return get(bytes, 0, bytes.length);
+  }
 
-		int end = offset + length;
-		for (int i = offset; i < end; i++) {
-			bytes[i] = get();
-		}
-		return this;
-	}
+  public ByteBuffer get(byte[] bytes, int offset, int length) {
+    if ((offset | length | (offset + length) | (bytes.length - (offset + length))) < 0) {
+      throw new IndexOutOfBoundsException();
+    }
 
-	public ByteBuffer order(ByteOrder order) {
-		return this;
-	}
+    if (length > remaining()) {
+      throw new BufferUnderflowException();
+    }
 
-	/***************************************************************
-	 * public char getChar()
-	 * @return 16 bit (UTF-16) char of ByteBuffer at this.position 
-	 * Caution: 8 or 32 bit character encodings are not supported.
-	 */
-	public char getChar() {
-        char res=getChar(this.position);
-        this.position+=2;
-        return res;
-	}
+    int end = offset + length;
+    for (int i = offset; i < end; i++) {
+      bytes[i] = get();
+    }
+    return this;
+  }
 
-	/***************************************************************
-	 * public char getChar(int pos)
-	 * @return 16 bit (UTF-16) char of ByteBuffer at int pos 
-	 * Caution: 8 or 32 bit character encodings are not supported.
-	 */
-	public char getChar(int pos) {
-		if (limit - pos < 2) {
-			throw new BufferUnderflowException();
-		}
-		int x1 = (array[pos]   & 0xff) << 8;
-		int x0 = (array[pos+1] & 0xff);
+  public ByteBuffer order(ByteOrder order) {
+    return this;
+  }
 
-		return (char) (x1 | x0);
-	}
+  /***************************************************************
+   * public char getChar()
+   * @return 16 bit (UTF-16) char of ByteBuffer at this.position
+   * Caution: 8 or 32 bit character encodings are not supported.
+   */
+  public char getChar() {
+    char res=getChar(this.position);
+    this.position+=2;
+    return res;
+  }
 
-	/***************************************************************
-	 * public ByteBuffer putChar(char c)
-	 * @return insert 16 bit (UTF-16) char c at this.position  
-	 * Caution: 8 or 32 bit character encodings are not supported.
-	 */
-	public ByteBuffer putChar(char c) {
-		if (limit - position < 2) {
-			throw new BufferOverflowException();
-		}
-		array[position]   = (byte)(c >> 8);
-		array[position+1] = (byte)(c     );
-		position += 2;
+  /***************************************************************
+   * public char getChar(int pos)
+   * @return 16 bit (UTF-16) char of ByteBuffer at int pos
+   * Caution: 8 or 32 bit character encodings are not supported.
+   */
+  public char getChar(int pos) {
+    if (limit - pos < 2) {
+      throw new BufferUnderflowException();
+    }
+    int x1 = (array[pos]   & 0xff) << 8;
+    int x0 = (array[pos+1] & 0xff);
 
-		return this;
-	}
+    return (char) (x1 | x0);
+  }
 
-	public int getInt() {
-		if (limit - position < 4) {
-			throw new BufferUnderflowException();
-		}
+  /***************************************************************
+   * public ByteBuffer putChar(char c)
+   * @return insert 16 bit (UTF-16) char c at this.position
+   * Caution: 8 or 32 bit character encodings are not supported.
+   */
+  public ByteBuffer putChar(char c) {
+    if (limit - position < 2) {
+      throw new BufferOverflowException();
+    }
+    array[position]   = (byte)(c >> 8);
+    array[position+1] = (byte)(c     );
+    position += 2;
 
-		int x3 = (array[position  ]       ) << 24;
-		int x2 = (array[position+1] & 0xff) << 16;
-		int x1 = (array[position+2] & 0xff) <<  8;
-		int x0 = (array[position+3] & 0xff);
-		position += 4;
+    return this;
+  }
 
-		return (x3 | x2 | x1 | x0);
-	}
+  public int getInt() {
+    if (limit - position < 4) {
+      throw new BufferUnderflowException();
+    }
 
-	public ByteBuffer putInt(int x) {
-		if (limit - position < 4) {
-			throw new BufferOverflowException();
-		}
+    int x3 = (array[position  ]       ) << 24;
+    int x2 = (array[position+1] & 0xff) << 16;
+    int x1 = (array[position+2] & 0xff) <<  8;
+    int x0 = (array[position+3] & 0xff);
+    position += 4;
 
-		array[position  ] = (byte)(x >> 24);
-		array[position+1] = (byte)(x >> 16);
-		array[position+2] = (byte)(x >>  8);
-		array[position+3] = (byte)(x      );
-		position += 4;
+    return (x3 | x2 | x1 | x0);
+  }
 
-		return this;
-	}
+  public ByteBuffer putInt(int x) {
+    if (limit - position < 4) {
+      throw new BufferOverflowException();
+    }
 
-	public long getLong() {
-		if (limit - position < 8) {
-			throw new BufferUnderflowException();
-		}
+    array[position  ] = (byte)(x >> 24);
+    array[position+1] = (byte)(x >> 16);
+    array[position+2] = (byte)(x >>  8);
+    array[position+3] = (byte)(x      );
+    position += 4;
 
-		long x7 = ((long)(array[position  ]       ) << 56);
-		long x6 = ((long)(array[position+1] & 0xff) << 48);
-		long x5 = ((long)(array[position+2] & 0xff) << 40);
-		long x4 = ((long)(array[position+3] & 0xff) << 32);
-		long x3 = ((long)(array[position+4] & 0xff) << 24);
-		long x2 = ((long)(array[position+5] & 0xff) << 16);
-		long x1 = ((long)(array[position+6] & 0xff) <<  8);
-		long x0 = (array[position+7] & 0xff      );
-		position += 8;
+    return this;
+  }
 
-		return (x7 | x6 | x5 | x4 | x3 | x2 | x1 | x0);
-	}
+  public long getLong() {
+    if (limit - position < 8) {
+      throw new BufferUnderflowException();
+    }
 
-	public ByteBuffer putLong(long x) {
-		if (limit - position < 8) {
-			throw new BufferOverflowException();
-		}
+    long x7 = ((long)(array[position  ]       ) << 56);
+    long x6 = ((long)(array[position+1] & 0xff) << 48);
+    long x5 = ((long)(array[position+2] & 0xff) << 40);
+    long x4 = ((long)(array[position+3] & 0xff) << 32);
+    long x3 = ((long)(array[position+4] & 0xff) << 24);
+    long x2 = ((long)(array[position+5] & 0xff) << 16);
+    long x1 = ((long)(array[position+6] & 0xff) <<  8);
+    long x0 = (array[position+7] & 0xff      );
+    position += 8;
 
-		array[position  ] = (byte)((x >> 56)       );
-		array[position+1] = (byte)((x >> 48) & 0xff);
-		array[position+2] = (byte)((x >> 40) & 0xff);
-		array[position+3] = (byte)((x >> 32) & 0xff);
-		array[position+4] = (byte)((x >> 24) & 0xff);
-		array[position+5] = (byte)((x >> 16) & 0xff);
-		array[position+6] = (byte)((x >>  8) & 0xff);
-		array[position+7] = (byte)((x      ) & 0xff);
-		position += 8;
+    return (x7 | x6 | x5 | x4 | x3 | x2 | x1 | x0);
+  }
 
-		return this;
-	}
+  public ByteBuffer putLong(long x) {
+    if (limit - position < 8) {
+      throw new BufferOverflowException();
+    }
 
-	@Override
-	public byte[] array() {
-		return array;
-	}
+    array[position  ] = (byte)((x >> 56)       );
+    array[position+1] = (byte)((x >> 48) & 0xff);
+    array[position+2] = (byte)((x >> 40) & 0xff);
+    array[position+3] = (byte)((x >> 32) & 0xff);
+    array[position+4] = (byte)((x >> 24) & 0xff);
+    array[position+5] = (byte)((x >> 16) & 0xff);
+    array[position+6] = (byte)((x >>  8) & 0xff);
+    array[position+7] = (byte)((x      ) & 0xff);
+    position += 8;
 
-	@Override
-	public boolean hasArray() {
-		return true;
-	}
+    return this;
+  }
 
-	public ByteBuffer compact() {
-		int pos = position();
-		int lim = limit();
-		int cap = capacity();
-		int rem = lim - pos;
+  @Override
+  public byte[] array() {
+    return array;
+  }
 
-		byte[] newArray = new byte[cap];
-		System.arraycopy(array, pos, newArray, 0, rem);
-		array = newArray;
+  @Override
+  public boolean hasArray() {
+    return true;
+  }
 
-		position(rem);
-		limit(cap);
-		return this;
-	}
+  public ByteBuffer compact() {
+    int pos = position();
+    int lim = limit();
+    int cap = capacity();
+    int rem = lim - pos;
 
-	public static ByteBuffer wrap(byte[] outMess) {
-		ByteBuffer byteBuffer = new ByteBuffer(outMess.length);
-		byteBuffer.clear();
-		System.arraycopy(outMess, 0 , byteBuffer.array, 0, outMess.length);
-		return byteBuffer;
-	}
+    byte[] newArray = new byte[cap];
+    System.arraycopy(array, pos, newArray, 0, rem);
+    array = newArray;
+
+    position(rem);
+    limit(cap);
+    return this;
+  }
+
+  public static ByteBuffer wrap(byte[] outMess) {
+    ByteBuffer byteBuffer = new ByteBuffer(-1, 0, outMess.length, outMess.length, new byte[outMess.length], 0);
+    byteBuffer.clear();
+    System.arraycopy(outMess, 0 , byteBuffer.array, 0, outMess.length);
+    return byteBuffer;
+  }
+
+  public boolean equals(Object ob) {
+    if (this == ob)
+      return true;
+    if (!(ob instanceof ByteBuffer))
+      return false;
+    ByteBuffer that = (ByteBuffer)ob;
+    int thisPos = this.position();
+    int thisRem = this.limit() - thisPos;
+    int thatPos = that.position();
+    int thatRem = that.limit() - thatPos;
+    if (thisRem < 0 || thisRem != thatRem)
+      return false;
+    return BufferMismatch.mismatch(this, thisPos,
+        that, thatPos,
+        thisRem) < 0;
+  }
+
+  Object base() {
+    return array;
+  }
 }
