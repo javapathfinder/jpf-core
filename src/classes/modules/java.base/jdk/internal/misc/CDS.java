bf7dad85e3ad6c518680dd0d177949b45badf4a1
eklaDFF <rahulkrsingh160086@gmail.com>
2024-05-24 14:38:26 +0530

    Create stub for CDS.java

---
+++ src/classes/modules/java.base/jdk/internal/misc/CDS.java
@@ -0,0 +1,11 @@
+package jdk.internal.misc;
+
+public class CDS {
+	public static void initializeFromArchive(Class<?> c){
+
+	}
+
+	public static long getRandomSeedForDumping(){
+		return 0L;
+	}
+}
