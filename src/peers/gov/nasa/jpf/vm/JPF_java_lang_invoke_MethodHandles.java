519c142c268db6ecffecf01c725e43a139e61276
eklaDFF <rahulkrsingh160086@gmail.com>
2024-07-21 16:11:55 +0530

    add new method findStaticVarHandle__Ljava_lang_Class_2Ljava_lang_String_2Ljava_lang_Class_2__Ljava_lang_invoke_VarHandle_2() to handle static field(only static field)

--- src/peers/gov/nasa/jpf/vm/JPF_java_lang_invoke_MethodHandles.java
+++ src/peers/gov/nasa/jpf/vm/JPF_java_lang_invoke_MethodHandles.java
@@ -57,6 +57,36 @@ public class JPF_java_lang_invoke_MethodHandles extends NativePeer {
       return jpfFieldVarHandle;
     }
 
+    @MJI
+    public int
+    findStaticVarHandle__Ljava_lang_Class_2Ljava_lang_String_2Ljava_lang_Class_2__Ljava_lang_invoke_VarHandle_2(
+            MJIEnv env, int objRef, int classRef, int stringRef, int typeRef) {
+      ClassInfo ci = env.getClassInfo(classRef);
+      int nameCls = env.getReferenceField(classRef, "name");
+      String clsName = env.getStringObject(nameCls);
+      ci = env.getSystemClassLoaderInfo().loadClass(clsName);
+      int nameType = env.getReferenceField(typeRef, "name");
+      String typeName = env.getStringObject(nameType);
+      String varName = env.getStringObject(stringRef);
+
+      // Checking access to non-static field, if found we will throw IllegalAccessException
+      FieldInfo fi = ci.getInstanceField(varName);
+      if (fi != null) {
+        env.throwException(IllegalAccessException.class.getName(), "Cannot access instance field: " + varName + " on class: " + clsName + " as static field");
+        return MJIEnv.NULL;
+      }
+
+      fi = ci.getStaticField(varName);
+      if (fi == null) {
+        env.throwException(NoSuchFieldException.class.getName(), "Cannot find field: " + varName + " on class: " + clsName);
+        return MJIEnv.NULL;
+      }
+      int jpfFieldVarHandle = env.newObject("java.base$&$java.lang.invoke.JPFFieldVarHandle");
+      env.setIntField(jpfFieldVarHandle, "fieldRef", fi.getFieldIndex());
+      env.setIntField(jpfFieldVarHandle, "classRef", classRef);
+      return jpfFieldVarHandle;
+    }
+
     private Class resolveTypeName(String typeName) throws ClassNotFoundException {
       Class type;
       if (typeName.equals("int")) {
