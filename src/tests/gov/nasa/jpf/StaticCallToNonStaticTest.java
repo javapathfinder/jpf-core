b75843970e2c899de0feadec0130a3f8ef437334
sanidhya00081 <nidhis.nt84@gmail.com>
2025-01-27 17:17:34 +0000

    New test is targetting the StaticCallToNonStaticTest()

--- src/tests/gov/nasa/jpf/StaticCallToNonStaticTest.java
+++ src/tests/gov/nasa/jpf/StaticCallToNonStaticTest.java
@@ -11,7 +11,7 @@ import java.io.File;
 public class StaticCallToNonStaticTest extends TestJPF {
 
     @Test
-public void testStaticCallToNonStatic() {
+    public void testStaticCallToNonStatic() {
   D.m();
 }
 
