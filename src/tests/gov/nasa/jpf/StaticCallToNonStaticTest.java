ab0e4b70dbf93f882da0f3c352a9f9af627fd7d7
sanidhya00081 <nidhis.nt84@gmail.com>
2025-01-30 15:28:06 +0000

    Removed the clean up method and moved settings.jason to .gitignore. Now, first the test fails with an error and later it passes with successful build.

--- src/tests/gov/nasa/jpf/StaticCallToNonStaticTest.java
+++ src/tests/gov/nasa/jpf/StaticCallToNonStaticTest.java
@@ -2,34 +2,16 @@ package gov.nasa.jpf;
 
 import org.junit.AfterClass;
 import gov.nasa.jpf.util.test.TestJPF;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-
-
-
 
 public class StaticCallToNonStaticTest extends TestJPF {
 
     @Test
     public void testStaticCallToNonStatic() {
-  D.m();
-}
-
-    // Cleanup method to delete the temporary file after tests
-    @AfterClass
-    public static void cleanUp() {
-        // Define the path to the temporary file
-        String tempDir = System.getProperty("java.io.tmpdir");
-        File tempFile = new File(tempDir, "tempFile.txt");
-
-        // Delete the temporary file after use
-        if (tempFile.exists()) {
-            boolean deleted = tempFile.delete();
-            if (deleted) {
-                System.out.println("Temporary file deleted successfully.");
-            } else {
-                System.out.println("Failed to delete temporary file.");
-            }
-        }
+        
+        Assertions.assertThrows(java.lang.IncompatibleClassChangeError.class, () -> {
+            D.m();  
+        });
     }
 }
