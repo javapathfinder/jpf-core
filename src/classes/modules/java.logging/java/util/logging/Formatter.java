660d25998c46c030563e6e7e02fbf06e1effdca6
Hritik Raj <hritik23154049@akgec.ac.in>
2025-12-19 13:53:21 +0530

    Add copyright headers to new files

--- src/classes/modules/java.logging/java/util/logging/Formatter.java
+++ src/classes/modules/java.logging/java/util/logging/Formatter.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ */
 package java.util.logging;
 public abstract class Formatter {
   public abstract String format(LogRecord record);
